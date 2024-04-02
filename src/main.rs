4b00b9bb9510d79ab49009ea9347a52fdafa1a42
ethan <irvinghu07@gmail.com>
2024-04-02 22:15:48 +0800

    ping

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener};
+use std::{
+    io::Write,
+    net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream},
+};
+
+fn handle_ping(stream: &mut TcpStream) {
+    stream.write_all(b"+PONG\r\n").unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 6379));
     let listener = TcpListener::bind(addr).unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => println!("New connection!"),
+    for streamr in listener.incoming() {
+        match streamr {
+            Ok(mut stream) => {
+                println!("New connection!");
+                handle_ping(&mut stream);
+            }
             Err(e) => println!("Error: {}", e),
         }
     }
