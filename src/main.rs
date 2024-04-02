a6af318ddbff782bc27430fb83177b17717e7d72
ethan <irvinghu07@gmail.com>
2024-04-02 21:09:30 +0800

    TCP socket binding

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 6379));
+    let listener = TcpListener::bind(addr).unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => println!("New connection!"),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
 }
