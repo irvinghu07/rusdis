4261df0553d63e39fde50264b019e859126a30e0
ethan <irvinghu07@gmail.com>
2024-04-02 23:29:41 +0800

    ping multiple times

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::{
-    io::Write,
+    io::{Read, Write},
     net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream},
 };
 
-fn handle_ping(stream: &mut TcpStream) {
-    stream.write_all(b"+PONG\r\n").unwrap();
+fn handle_ping(stream: &mut TcpStream, buffer: &mut [u8; 1 << 10]) {
+    loop {
+        let sz: usize = stream.read(buffer).unwrap();
+        if sz == 0 {
+            println!("Client disconnected");
+            return;
+        }
+        println!("Recieved {} bytes", sz);
+        stream.write_all(b"+PONG\r\n").unwrap();
+        stream.flush().unwrap();
+    }
 }
 
 fn main() {
@@ -12,11 +21,13 @@ fn main() {
     println!("Logs from your program will appear here!");
     let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 6379));
     let listener = TcpListener::bind(addr).unwrap();
+    println!("Listening on {}:{}", addr.ip(), addr.port());
+    let mut read_buffer = [0; 1 << 10];
     for streamr in listener.incoming() {
         match streamr {
             Ok(mut stream) => {
                 println!("New connection!");
-                handle_ping(&mut stream);
+                handle_ping(&mut stream, &mut read_buffer);
             }
             Err(e) => println!("Error: {}", e),
         }
