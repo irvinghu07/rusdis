48d8ab889c3e61cd563d17a92d6bf56c9a0717fc
ethan <irvinghu07@gmail.com>
2024-04-03 00:44:15 +0800

    Refactoring into Tokio async

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
-use std::{
-    io::{Read, Write},
-    net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream},
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
 };
 
-fn handle_ping(stream: &mut TcpStream, buffer: &mut [u8; 1 << 10]) {
+const PONG: &[u8] = b"+PONG\r\n";
+
+async fn handle_ping(
+    buffer: &mut [u8; 1 << 10],
+    stream: &mut TcpStream,
+) -> Result<(), Box<dyn std::error::Error>> {
     loop {
-        let sz: usize = stream.read(buffer).unwrap();
-        if sz == 0 {
-            println!("Client disconnected");
-            return;
+        match stream.read(buffer).await {
+            Ok(0) => return Ok(()),
+            Ok(n) => {
+                println!("Recieved {} bytes", n);
+                stream.write_all(PONG).await?;
+                stream.flush().await?;
+            }
+            Err(e) => eprintln!("Error reading from socket: {}", e),
         }
-        println!("Recieved {} bytes", sz);
-        stream.write_all(b"+PONG\r\n").unwrap();
-        stream.flush().unwrap();
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 6379));
-    let listener = TcpListener::bind(addr).unwrap();
+    let listener = TcpListener::bind(addr).await?;
     println!("Listening on {}:{}", addr.ip(), addr.port());
-    let mut read_buffer = [0; 1 << 10];
-    for streamr in listener.incoming() {
-        match streamr {
-            Ok(mut stream) => {
-                println!("New connection!");
-                handle_ping(&mut stream, &mut read_buffer);
-            }
-            Err(e) => println!("Error: {}", e),
-        }
+    let mut buffer = [0; 1 << 10];
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            handle_ping(&mut buffer, &mut stream).await.unwrap();
+        });
     }
 }
